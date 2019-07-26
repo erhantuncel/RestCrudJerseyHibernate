ee25c81a9535917d72b8d4c8221ab837046240cc
Erhan TUNCEL <yazilim.erhan@gmail.com>
2019-07-26 21:09:27 +0300

    Initial commit

---
+++ src/main/java/com/erhan/rest/MyResource.java
@@ -0,0 +1,31 @@
+package com.erhan.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+	Logger logger = LogManager.getLogger(MyResource.class);
+	
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+    	logger.info("getResource is invoked.");
+        return "Got it!";
+    }
+}
